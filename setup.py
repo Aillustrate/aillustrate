227f55e75aab69d0620720e72d70a6b19384da73
Анна Маршалова <78635473+anna-marshalova@users.noreply.github.com>
2024-01-18 01:25:47 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,18 @@ import io
 import os
 from setuptools import find_packages, setup
 
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    """
 
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+    
 def read_requirements(path):
     return [
         line.strip()
@@ -16,7 +27,7 @@ setup(
     version="0.0.1",
     description="automatic image generation pipeline",
     url="https://github.com/Aillustrate/wonderslide-interior-generation/",
-    long_description=open('README.md').read(),
+    long_description=read('README.md'),
     long_description_content_type="text/markdown",
     author="Aillustrate team",
     packages=find_packages(exclude=["tests", ".github"]),
